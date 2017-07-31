b07478538c937beb7aafcaab2e67bd947e08f931
zixcon <zixcon.fan@outlook.com>
2017-07-31 18:11:06 +0800

    Create sparser.go

---
+++ go-spyder/src/util/sparser.go
@@ -0,0 +1,67 @@
+package wangyi
+
+import (
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"strings"
+)
+
+func HtmlTitle(body string) []string {
+	var titles []string
+	bodyReader := strings.NewReader(body)
+	doc, err := goquery.NewDocumentFromReader(bodyReader)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	doc.Find(".table_bg001 ").EachWithBreak(func(i int, s *goquery.Selection) bool {
+		// 标题
+		th := s.Find("thead").Find("th")
+		log.Println("标题Size:" , th.Size())
+		arr := make([]string, th.Size())
+		th.EachWithBreak(func(j int, g *goquery.Selection) bool {
+			th_title := g.Text()
+			arr[j] = th_title
+			if (j >= th.Size()) {
+				return false
+			} else {
+				return true
+			}
+		})
+		titles = arr
+		return false
+	})
+	return titles
+}
+
+func HtmlContent(body string) map[string][]string {
+	var content map[string][]string
+	var key string
+	bodyReader := strings.NewReader(body)
+	doc, err := goquery.NewDocumentFromReader(bodyReader)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	/* 创建集合 */
+	content = make(map[string][]string)
+	doc.Find(".table_bg001 ").EachWithBreak(func(i int, s *goquery.Selection) bool{
+		// 内容
+		s.Find("tr").Each(func(j int, g *goquery.Selection) {
+			td := g.Find("td")
+			size := td.Size()
+			body := make([]string, size )
+			td.Each(func(k int, h *goquery.Selection) {
+				td_body := h.Text()
+				if k == 0 {
+					key = td_body
+				} else {
+					body[k-1] = td_body
+				}
+				//fmt.Print(td_body + " ")
+			})
+			content[key] = body
+			//fmt.Println()
+		})
+		return false
+	})
+	return content
+}
