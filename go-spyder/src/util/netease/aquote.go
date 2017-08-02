0716e193c37ba1ad4df0d89440d907009195d58e
zixcon <zixcon.fan@outlook.com>
2017-08-02 21:28:32 +0800

    Update aquote.go

--- go-spyder/src/util/netease/aquote.go
+++ go-spyder/src/util/netease/aquote.go
@@ -4,25 +4,42 @@ import (
 	"strings"
 	"github.com/PuerkitoBio/goquery"
 	"log"
+	"util/client"
 )
 
-var url = "http://quotes.money.163.com/"
+var url = "http://quotes.money.163.com"
 
-func DealQuote(quotes []*Quote) {
-	for i := 0; i < quotes; i++ {
-		quote := quotes[i]
-		url + quote.CODE + ".html"
+func WY_Aquote_Header() map[string]string {
+	header := map[string]string{
+		"Host":                      "quotes.money.163.com",
+		"Accept-Language":           "zh-CN,zh;q=0.8",
+		"Connection":                "keep-alive",
+		"Cache-Control":             "max-age=0",
+		"Upgrade-Insecure-Requests": "1",
+		"User-Agent":                RadomUA(),
+		"Accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
 	}
+	return header
+}
+
+func DealQuote(quotes []Quote) []string {
+	arr := make([]string, len(quotes))
+	for i := 0; i < len(quotes); i++ {
+		quote := quotes[i]
+		arr[i] = url + "/" + quote.CODE + ".html"
+	}
+	return arr
 }
 
 func forOneUrl(body string) string {
+	//log.Println(body)
 	bodyReader := strings.NewReader(body)
 	doc, err := goquery.NewDocumentFromReader(bodyReader)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 	var href string
-	doc.Find(".area").Find(".title_01").EachWithBreak(func(i int, s *goquery.Selection) bool {
+	doc.Find("#menuCont").Find(".submenu_cont").Find(".sub_menu").EachWithBreak(func(i int, s *goquery.Selection) bool {
 		s.Find("ul").Find("li").EachWithBreak(func(i int, s *goquery.Selection) bool {
 			if strings.EqualFold("历史交易数据", s.Text()) {
 				href, _ = s.Find("a").Attr("href")
@@ -30,8 +47,29 @@ func forOneUrl(body string) string {
 			}
 			return true
 		})
-		return false
+		return true
 	})
 	log.Println(href)
-	return href
+	return url + href
+}
+
+func CallAquote(url string) []byte {
+	log.Println()
+	req := client.GetRequest(url, nil, WY_Aquote_Header())
+	body := client.DoReqeust(req)
+	//log.Println(body)
+	return body
+}
+
+// 历史交易数据url
+func DoAquote(quotes []Quote) []string {
+	var arr = make([]string, len(quotes))
+	aquoteArr := DealQuote(quotes)
+	for i := 0; i < len(aquoteArr); i++ {
+		body := CallAquote(aquoteArr[i])
+		url := forOneUrl(string(body))
+		log.Println(url)
+		arr[i] = url
+	}
+	return arr
 }
